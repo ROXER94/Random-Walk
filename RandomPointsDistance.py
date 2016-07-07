578307c8eeb5cbb1f30c49f38308e92f66c351a2
Robert Oxer <rdo26@cornell.edu>
2016-07-07 18:45:21 -0400

    Update RandomPointsDistance.py

--- RandomPointsDistance.py
+++ RandomPointsDistance.py
@@ -1,18 +1,62 @@
-#Prints the average distance of two random points in a 1x1 square
-
-import math,random
-
-def distance(x1,y1,x2,y2):
-	X = x2 - x1
-	Y = y2 - y1
-	return math.sqrt(X*X + Y*Y)
-
-sum = 0
-attempts = 100000
-
-for i in range(attempts):
-	sum = sum + distance(random.random(),random.random(),random.random(),random.random())
-
-
-print(sum/attempts)
-#Approximation of (2+sqrt(2) +5ln(sqrt(2)+1))/15 = 0.521405433164720678330982
+The average waiting time between consecutive Heads-Heads (HH) is longer than the average waiting time between Heads-Tails(HT)
+import random
+length = 50000
+string = ""
+HH = 0
+HT = 0
+index = 1
+HHarray = []
+HTarray = []
+consecutive = False
+for i in range(length):
+ if random.random() < .5:
+ string += "H"
+ else:
+ string += "T"
+for i, j in zip(string[::1], string[1::1]):
+ if i+j == "HH" and consecutive == False:
+ HHarray.append(index)
+ index += 1
+ consecutive = True
+ elif i+j == "HT":
+ HTarray.append(index)
+ index += 1
+ consecutive = False
+ else:
+ index += 1
+ consecutive = False
+HHarray = [HHarray[0]] + [j-i for i, j in zip(HHarray[:-1], HHarray[1:])]
+HTarray = [HTarray[0]] + [j-i for i, j in zip(HTarray[:-1], HTarray[1:])] 
+print("The average waiting time of HH is " + str(sum(HHarray)/len(HHarray)))#Expected Value is 6
+print("The average waiting time of HT is " + str(sum(HTarray)/len(HTarray)))#Expected Value is 4
+#The average waiting time between consecutive Heads-Heads (HH) is longer than the average waiting time between Heads-Tails(HT)
+import random
+length = 50000
+string = ""
+HH = 0
+HT = 0
+index = 1
+HHarray = []
+HTarray = []
+consecutive = False
+for i in range(length):
+ if random.random() < .5:
+ string += "H"
+ else:
+ string += "T"
+for i, j in zip(string[::1], string[1::1]):
+ if i+j == "HH" and consecutive == False:
+ HHarray.append(index)
+ index += 1
+ consecutive = True
+ elif i+j == "HT":
+ HTarray.append(index)
+ index += 1
+ consecutive = False
+ else:
+ index += 1
+ consecutive = False
+HHarray = [HHarray[0]] + [j-i for i, j in zip(HHarray[:-1], HHarray[1:])]
+HTarray = [HTarray[0]] + [j-i for i, j in zip(HTarray[:-1], HTarray[1:])] 
+print("The average waiting time of HH is " + str(sum(HHarray)/len(HHarray)))#Expected Value is 6
+print("The average waiting time of HT is " + str(sum(HTarray)/len(HTarray)))#Expected Value is 4
